6542aa1d0661fd8c07f86b587d7e6fa0d48bba4b
Arbalester <st43189@student.upce.cz>
2021-03-28 16:34:19 +0200

    Background optimalization.

--- NNPG2_cv4/Background.cs
+++ NNPG2_cv4/Background.cs
@@ -8,27 +8,28 @@ namespace NNPG2_cv4
         public Color Color 
         { 
             get { return color; } 
-            set { color = value; renderImage = false; } 
+            set { color = value; brush = new SolidBrush(color); } 
         }
         public Image Image
         {
             get { return image; }
-            set { image = value; renderImage = true; }
+            set { image = value; brush = new TextureBrush(image, WrapMode.Tile); }
         }
 
         private Color color;
+
         private Image image;
-        private bool renderImage;
+        private Brush brush;
 
         public Background(Color color)
         {
             this.color = color;
+            brush = new SolidBrush(color);
         }
 
         public void Render(Graphics g, int width, int height)
         {
-            if (renderImage) g.FillRectangle(new TextureBrush(image, WrapMode.Tile), new Rectangle(0, 0, width, height));
-            else g.FillRectangle(new SolidBrush(Color), new Rectangle(0, 0, width, height));             
+            g.FillRectangle(brush, new Rectangle(0, 0, width, height));          
         }
     }
 }
